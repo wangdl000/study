a0deff2f7881f8202e245a4c73a518ca054ca4b1
Dianlang <wangdl000@yahoo.co.jp>
2018-11-05 23:02:01 +0900

    人如果没有梦想，和咸鱼有什么区别

---
+++ 01_Java_Bisic_Code/FirstJavaProject/src/Person.java
@@ -0,0 +1,47 @@
+
+public class Person {
+
+	private String name;
+
+	private int age;
+
+	private String sex;
+
+	private String adrress;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAdrress() {
+		return adrress;
+	}
+
+	public void setAdrress(String adrress) {
+		this.adrress = adrress;
+	}
+
+
+
+
+}
